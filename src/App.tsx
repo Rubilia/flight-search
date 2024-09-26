b4df101a132ce4816c350533aadf3e55ce2bf85b
Ilia Rubashkin <rubiliam2003@gmail.com>
2024-09-26 16:55:01 -0700

    Added image below header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import Header from './components/Header';
-
+import Header from "./components/Header";
+import ImageSection from "./components/ImageSection";
 
 function App() {
   return (
     <div>
       <Header />
+      <ImageSection />
     </div>
-  )
+  );
 }
 
 export default App;
---
+++ src/components/ImageSection.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
+import flightsImage from "../assets/images/flights_image.svg";
+
+function ImageSection() {
+  return (
+    <Box
+      sx={{
+        marginLeft: "10%",
+        marginRight: "10%",
+        textAlign: "center",
+        position: "relative",
+      }}
+    >
+      <img
+        src={flightsImage}
+        alt="Flights"
+        style={{ width: "100%", height: "auto" }}
+      />
+
+      <Typography
+        variant="h4"
+        component="div"
+        sx={{
+          position: "absolute",
+          bottom: "10%",
+          left: "50%",
+          transform: "translateX(-50%)",
+          color: "#202124",
+          fontFamily: "Roboto, sans-serif",
+          fontWeight: 400,
+          fontSize: "56px",
+          padding: "8px 16px",
+          borderRadius: "4px",
+        }}
+      >
+        Flights
+      </Typography>
+    </Box>
+  );
+}
+
+export default ImageSection;
